7750614539b461cbfd699142ddede9348ab4d60b
Hamza Saktiwy <hsaktiwy@gmail.com>
2026-04-20 09:55:42 +0100

    Part1 properly worked

--- Part1/scripts/control-plane.sh
+++ Part1/scripts/control-plane.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 echo "Installing K3s on the control plane node..."
-echo "Installing K3s..."
-curl -sfL https://get.k3s.io | sh INSTALL_K3S_EXEC="--node-ip 192.168.56.110 --bind-address 192.168.56.110" -
-mkdir -p /home/vagrant/.kube
-sud`o cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown -R vagrant:vagrant /home/vagrant/.kube/config
-# Get the token for the worker nodes
-TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
 
-# Store the token for the workers to use`
-echo $TOKEN > /vagrant/token
+# Force K3s to bind to the private network IP instead of the NAT IP
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.110 --bind-address 192.168.56.110" sh -
+
+# --- THE PERMISSIONS FIX ---
+# Create the .kube folder, copy the root config, and give the vagrant user full ownership
+mkdir -p /home/vagrant/.kube
+cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+chown -R vagrant:vagrant /home/vagrant/.kube
+
+# Automatically point kubectl to the unlocked config file every time the user logs in
+echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
+# ---------------------------
+
+# Share the token so the worker node can join
+cp /var/lib/rancher/k3s/server/node-token /vagrant/token
