19e6dd583b9cd162512f10ccb0030b0e3ec2460c
Hamza Saktiwy <hsaktiwy@gmail.com>
2026-06-10 18:04:21 +0100

    Second part properly created

--- p1/scripts/control-plane.sh
+++ p1/scripts/control-plane.sh
@@ -2,7 +2,7 @@
 echo "Installing K3s on the control plane node..."
 
 # Force K3s to bind to the private network IP instead of the NAT IP
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110 --bind-address 192.168.56.110 --tls-san=192.168.1.50 --flannel-iface=eth1" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110 --bind-address 192.168.56.110 --tls-san=192.168.1.110  --flannel-iface=eth1" sh -
 
 # --- THE PERMISSIONS FIX ---
 # Create the .kube folder, copy the root config, and give the vagrant user full ownership
@@ -16,6 +16,3 @@ echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
 
 # Share the token so the worker node can join
 cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/token
-
-# INSTALL IFCONFIG FOR DEBUGING
-#apt-get install net-tools -y
